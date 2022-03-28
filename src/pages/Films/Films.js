f7872e713ef7c65a76ae63cfda3e02c53b5f7e2c
bui khoa <buikhoa793@gmail.com>
2022-03-28 20:53:27 +0300

    UpdateAdminLayout

--- src/pages/Films/Films.js
+++ src/pages/Films/Films.js
@@ -1,100 +1,131 @@
-import React from 'react'
-import { Table } from 'antd';
-import { Input } from 'antd';
-import {FileSearchOutlined} from '@ant-design/icons'
-const columns = [
-    {
-      title: 'Name',
-      dataIndex: 'name',
-      filters: [
-        {
-          text: 'Joe',
-          value: 'Joe',
-        },
-        {
-          text: 'Category 1',
-          value: 'Category 1',
-        },
-        {
-          text: 'Category 2',
-          value: 'Category 2',
-        },
-      ],
-      filterMode: 'tree',
-      filterSearch: true,
-      onFilter: (value, record) => record.address.startsWith(value),
-      width: '30%',
-    },
-    {
-      title: 'Age',
-      dataIndex: 'age',
-      sorter: (a, b) => a.age - b.age,
-    },
-    {
-      title: 'Address',
-      dataIndex: 'address',
-      filters: [
-        {
-          text: <span>London</span>,
-          value: 'London',
-        },
-        {
-          text: <span>New York</span>,
-          value: 'New York',
-        },
-      ],
-      onFilter: (value, record) => record.address.startsWith(value),
-      filterSearch:(input, record) => record.value.indexOf(input) > -1,
-      width: '40%',
-    },
-  ];
-  
-  const data = [
-    {
-      key: '1',
-      name: 'John Brown',
-      age: 32,
-      address: 'New York No. 1 Lake Park',
-    },
-    {
-      key: '2',
-      name: 'Jim Green',
-      age: 42,
-      address: 'London No. 1 Lake Park',
-    },
-    {
-      key: '3',
-      name: 'Joe Black',
-      age: 32,
-      address: 'Sidney No. 1 Lake Park',
-    },
-    {
-      key: '4',
-      name: 'Jim Red',
-      age: 32,
-      address: 'London No. 2 Lake Park',
-    },
-  ];
-  
-  function onChange(pagination, filters, sorter, extra) {
-    console.log('params', pagination, filters, sorter, extra);
-  }
-  const { Search } = Input;
-
+import React, { Fragment, useEffect } from "react";
+import { Button, Table } from "antd";
+import { Input } from "antd";
+import {
+  DeleteOutlined,
+  EditOutlined,
+  SearchOutlined,
+} from "@ant-design/icons";
+import { useDispatch, useSelector } from "react-redux";
+import { getFilmAction } from "../../actions/ManageFilmAction";
+import { uniqueId } from "lodash";
+import { NavLink } from "react-router-dom";
 
 export default function Films() {
-    return (
-        <div >
-            <h3 className='text-4xl'>Manage Films</h3>
-            <Search
-      placeholder="input search text"
-      className= 'mb-5'
-      allowClear
-      enterButton="Search"
-      size="large"
-      onSearch={<FileSearchOutlined />}
-    />
-            <Table columns={columns} dataSource={data} onChange={onChange} />
-        </div>
-    )
+  const { arrFilmDefault } = useSelector((state) => state.MovieListReducer);
+  const dispatch = useDispatch();
+  console.log("arrFilmDefault", arrFilmDefault);
+  useEffect(() => {
+    dispatch(getFilmAction());
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+  const columns = [
+    {
+      title: "Id",
+      dataIndex: "maPhim",
+      value: (text, object) => {
+        return <span key={uniqueId}>{text}</span>;
+      },
+      sorter: (a, b) => a.maPhim - b.maPhim,
+      sortDirection: ["descend", "ascend"],
+      width: "20%",
+    },
+    {
+      title: "Image",
+      dataIndex: "age",
+      render: (text, film, index) => {
+        return (
+          <Fragment key={index}>
+            <img
+              src={film.hinhAnh}
+              alt={film.tenPhim}
+              width={100}
+              height={100}
+              onError={(e) => {
+                e.target.onError = null;
+                e.target.src = `https://picsum.photos/id/${index}/100/100`;
+              }}
+            />
+          </Fragment>
+        );
+      },
+      width: "20%",
+    },
+    {
+      title: "Name Movie",
+      dataIndex: "tenPhim",
+      sorter: (a, b) => {
+        let tenPhimA = a.tenPhim.toLowerCase();
+        let tenPhimB = b.tenPhim.toLowerCase();
+        if (tenPhimA > tenPhimB) {
+          return 1;
+        }
+        return -1;
+      },
+      width: "20%",
+      sortDirection: ["descend", "ascend"],
+    },
+    {
+      title: "Description",
+      dataIndex: "tenPhim",
+      sorter: (a, b) => {
+        let moTaA = a.moTa.toLowerCase();
+        let moTaB = b.moTa.toLowerCase();
+        if (moTaA > moTaB) {
+          return 1;
+        }
+        return -1;
+      },
+      render: (text, film) => {
+        return (
+          <Fragment key={film}>
+            {film.moTa.length > 50
+              ? film.moTa.substr(0, 50) + " ..."
+              : film.moTa}
+          </Fragment>
+        );
+      },
+      sortDirection: ["descend", "ascend"],
+      width: "20%",
+    },
+    {
+      title: "Action",
+      dataIndex: "hanhDong",
+      render: (text, film) => {
+        return (
+          <Fragment key={film}>
+            <NavLink key={film} className="mr-2 text-2xl" to="/">
+              <EditOutlined style={{ color: "blue" }} />
+            </NavLink>
+            <NavLink key={film} className=" text-2xl" to="/">
+              <DeleteOutlined style={{ color: "red" }} />
+            </NavLink>
+          </Fragment>
+        );
+      },
+      width: "20%",
+      sortDirection: ["descend", "ascend"],
+    },
+  ];
+  const data = arrFilmDefault;
+  const onSearch = (value) => console.log(value);
+  function onChange(pagination, filters, sorter, extra) {
+    console.log("params", pagination, filters, sorter, extra);
+  }
+  const { Search } = Input;
+  return (
+    <div>
+      <h3 className="text-4xl">Manage Films</h3>
+      <Button>Adding Film</Button>
+      <Search
+        placeholder="Search here"
+        className="mb-5 "
+        allowClear
+        enterButton={<SearchOutlined className="mb-2" />}
+        size="large"
+        onSearch={onSearch}
+      />
+      <Table columns={columns} dataSource={data} onChange={onChange} />
+    </div>
+  );
 }
--- src/templates/HomeTemplate/Layout/Header/Header.js
+++ src/templates/HomeTemplate/Layout/Header/Header.js
@@ -2,54 +2,67 @@
 import React, { Fragment } from "react";
 import { NavLink } from "react-router-dom";
 import { history } from "../../../../App";
-import { Select } from 'antd';
-import _ from 'lodash'
-import {useTranslation} from 'react-i18next'
+import { Select } from "antd";
+import _ from "lodash";
+import { useTranslation } from "react-i18next";
 import { useSelector } from "react-redux";
 import { USER_LOGIN, TOKEN } from "../../../../util/settings/config";
 export default function Header(props) {
-  const {t, i18n} = useTranslation();
+  const { t, i18n } = useTranslation();
   const { Option } = Select;
-  const handleChange =(value) => {
-       i18n.changeLanguage(value)
-  }
-  const {userLogin} = useSelector(state => state.ManageUserReducer)
-   const renderLogin = () => {
-     if(_.isEmpty(userLogin)) {
-        return <Fragment>
-                   <button
-              onClick={() => {
-                history.push("/login");
-              }}
-              className="self-center px-8 py-3 rounded"
-            >
-              {t('Sign In')}
-            </button>
-            <button className="self-center px-8 py-3 font-semibold rounded dark:text-coolGray-50"   onClick={() => {
-                history.push("/register");
-              }}>
-            {t('Sign Up')}
-            </button>
-            
+  const handleChange = (value) => {
+    i18n.changeLanguage(value);
+  };
+  const { userLogin } = useSelector((state) => state.ManageUserReducer);
+  const renderLogin = () => {
+    if (_.isEmpty(userLogin)) {
+      return (
+        <Fragment>
+          <button
+            onClick={() => {
+              history.push("/login");
+            }}
+            className="self-center px-8 py-3 rounded"
+          >
+            {t("Sign In")}
+          </button>
+          <button
+            className="self-center px-8 py-3 font-semibold rounded dark:text-coolGray-50"
+            onClick={() => {
+              history.push("/register");
+            }}
+          >
+            {t("Sign Up")}
+          </button>
         </Fragment>
-     } else {
-       return  <Fragment> <button
-       onClick={() => {
-         history.push("/profile");
-       }}
-       className="self-center px-8 py-3 rounded"
-     >
-       Hello {userLogin.taiKhoan}
-     </button>
-         <button className='text-white mr-5' onClick={(()=>{
+      );
+    } else {
+      return (
+        <Fragment>
+          {" "}
+          <button
+            onClick={() => {
+              history.push("/profile");
+            }}
+            className="self-center px-8 py-3 rounded"
+          >
+            Hello {userLogin.taiKhoan}
+          </button>
+          <button
+            className="text-white mr-5"
+            onClick={() => {
               localStorage.removeItem(USER_LOGIN);
               localStorage.removeItem(TOKEN);
-              history.push('/home');
+              history.push("/home");
               window.location.reload();
-          })}>Sign Out</button>
-     </Fragment> 
-     }
-   }
+            }}
+          >
+            Sign Out
+          </button>
+        </Fragment>
+      );
+    }
+  };
   return (
     <div>
       <header className="p-4 dark:bg-coolGray-800  bg-opacity-60 bg-black text-white fixed w-full z-10">
@@ -76,7 +89,7 @@ export default function Header(props) {
                 className="flex items-center px-4 -mb-1 dark:border-transparent dark:border-emerald-400 text-white"
                 activeClassName="border-b-2 border-white"
               >
-                {t('Home')}
+                {t("Home")}
               </NavLink>
             </li>
             <li className="flex">
@@ -87,7 +100,7 @@ export default function Header(props) {
                 className="flex items-center px-4 -mb-1 dark:border-transparent dark:border-emerald-400 text-white"
                 activeClassName="border-b-2 border-white"
               >
-               {t('Contact')}
+                {t("Contact")}
               </NavLink>
             </li>
             <li className="flex">
@@ -98,18 +111,21 @@ export default function Header(props) {
                 activeClassName="border-b-2 border-white"
                 to="/news"
               >
-                {t('News')}
+                {t("News")}
               </NavLink>
             </li>
           </ul>
           <div className="items-center flex-shrink-0 hidden lg:flex">
             {renderLogin()}
 
-
-            <Select defaultValue="vi" style={{ width: 100 }} onChange={handleChange}>
-            <Option value="eng">Eng </Option>
-            <Option value="fin">Fin </Option>
-            <Option value="vi">Vi </Option>
+            <Select
+              defaultValue="vi"
+              style={{ width: 100 }}
+              onChange={handleChange}
+            >
+              <Option value="eng">Eng </Option>
+              <Option value="fin">Fin </Option>
+              <Option value="vi">Vi </Option>
             </Select>
           </div>
           <button className="p-4 lg:hidden">
